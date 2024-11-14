2d3c5801e54d4fe6ab81a395185be9566f12a8e2
minsu kim <gnffk48@naver.com>
2024-11-14 22:52:20 +0900

    OverWorldPlayer 수정

--- Cuphead/include/OverWorldPlayer.hpp
+++ Cuphead/include/OverWorldPlayer.hpp
@@ -14,7 +14,6 @@ public:
 	virtual void update() override;
 	virtual void render(HDC hdc) override;
 
-	void CreateProjectile();
 
 	virtual void OnCollision(Object* other) override;
 	virtual void OnCollisionEntry(Object* other) override;
--- Cuphead/src/OverWorldPlayer.cpp
+++ Cuphead/src/OverWorldPlayer.cpp
@@ -4,17 +4,16 @@
 #include "InputDeviceHandler.hpp"
 #include "Texture.hpp"
 #include "ResourceHandler.hpp"
-#include "Projectile.hpp"
 #include "func.hpp"
 
 OverWorldPlayer::OverWorldPlayer() {
 	CreateCollider();
 	getCollider()->setScale(Vec2{ 100.f, 100.f });
 
-	auto tex = ResourceHandler::GetInst().LoadTexture(L"Player_Texture", L"/texture/idle/cuphead_idle.png");
+	auto tex = ResourceHandler::GetInst().LoadTexture(L"OverWorldPlayer_Texture", L"texture/overworld/Cuphead");
 	CreateAnimator();
-	getAnimator()->createAnimation(L"Player_Idle", tex, Vec2{ 0.f, 0.f }, Vec2{ 100.f, 155.f }, Vec2{ 100.f, 0.f }, 0.065f, 9);
-	getAnimator()->play(L"Player_Idle");
+	getAnimator()->createAnimation(L"OverWorldPlayer_Idle", tex, Vec2{ 0.f, 550.f }, Vec2{ 100.f, 110.f }, Vec2{ 50.f, 0.f }, 0.065f, 9);
+	getAnimator()->play(L"OverWorldPlayer_Idle");
 }
 
 OverWorldPlayer::~OverWorldPlayer()
@@ -35,9 +34,6 @@ void OverWorldPlayer::update() {
 	if (KEY_HOLD(InputData::DOWN)) {
 		objPos.y += 200.f * fDT;
 	}
-	if (KEY_TAP(InputData::Z)) {
-		CreateProjectile();
-	}
 
 	setObjPos(objPos);
 }
@@ -46,18 +42,6 @@ void OverWorldPlayer::render(HDC hdc) {
 	componentRender(hdc);
 }
 
-void OverWorldPlayer::CreateProjectile() {
-	auto playerPos = getObjPos();
-	playerPos.y -= getObjScale().y / 2.f;
-
-	Projectile* projectile = new Projectile{ };
-	projectile->setObjName(L"Player_Projectile");
-	projectile->setObjPos(playerPos);
-	projectile->setObjScale(Vec2{ 30.f, 30.f });
-	projectile->setDirection(Vec2(0.f, 1.f));
-
-	CreateObject(GROUP_TYPE::PLAYER_PROJECTILE, projectile);
-};
 
 void OverWorldPlayer::OnCollision(Object* other)
 {
