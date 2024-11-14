5255442b711da4695ded4df5ac429db247958c99
orange2458 <asm1133@naver.com>
2024-11-15 02:24:19 +0900

    network 기능에 예외 처리 기능 추가, 사용자의 편의를 제공하기 위해 Ipv4Addr 클래스 구현

---
+++ server/Network.hpp
@@ -0,0 +1,75 @@
+#ifndef NETWORK_HPP
+#define NETWORK_HPP
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32")
+
+#include <string>
+#include <string_view>
+#include <cstdint>
+
+#include "MyException.hpp"
+
+using namespace std::literals;
+
+#define NET_NOEXCEPT noexcept
+#define NET_EXCEPT(error, desc) network::Exception(__LINE__, __FILE__, error, desc)
+#define NET_LAST_EXCEPT(desc) NET_EXCEPT(WSAGetLastError(), desc)
+
+namespace network {
+
+	void initNet( );
+	void closeNet( );
+	
+	class Exception : public MyException {
+	public:
+		Exception( int line, const char* file, int error, std::string_view desc ) NET_NOEXCEPT;
+
+		const char* type( ) const NET_NOEXCEPT override {
+			return "Network Exception";
+		}
+		int error( ) const NET_NOEXCEPT {
+			return error_;
+		}
+
+	private:
+		int error_;
+	};
+
+	class Ipv4Addr {
+	public:
+		Ipv4Addr( std::string_view ip = "127.0.0.1"sv )
+			: present_( ip ), addr_( ) {
+			if ( inet_pton( AF_INET, present_.data( ), &addr_ ) != 1 ) {
+				throw NET_LAST_EXCEPT( "Failed to convert IPv4 address"sv );
+			}
+		}
+
+		Ipv4Addr( std::uint32_t addr )
+			: present_( 15u, '\0' ), addr_( addr ) {
+			if ( inet_ntop( AF_INET, &addr_, present_.data( ), present_.size( ) ) == nullptr ) {
+				throw NET_LAST_EXCEPT( "Failed to convert IPv4 address"sv );
+			}
+		}
+
+		Ipv4Addr( const sockaddr& addr ) NET_NOEXCEPT
+			: present_( ), addr_( reinterpret_cast<const sockaddr_in*>( &addr )->sin_addr.s_addr ) {}
+		
+		Ipv4Addr( const sockaddr_in& addr ) NET_NOEXCEPT
+			: present_( ), addr_( addr.sin_addr.s_addr ) {}
+
+		std::string_view present( ) const NET_NOEXCEPT {
+			return present_;
+		}
+		std::uint32_t get( ) const NET_NOEXCEPT {
+			return addr_;
+		}
+
+	private:
+		std::string present_;
+		std::uint32_t addr_;
+	};
+}
+
+#endif // NETWORK_HPP
