7e448d7514bef3c23032e73f6c76eee543507c25
orange2458 <asm1133@naver.com>
2024-11-26 22:21:06 +0900

    server의 accept, send, recv 구조 구현

--- server/Network.cpp
+++ server/Network.cpp
@@ -3,13 +3,12 @@
 
 namespace network {
 
-	void network::initNet( ) {
-	}
-
-	void network::closeNet( ) {
-	}
-
 	Exception::Exception( int line, const char* file, int error, std::string_view desc ) NET_NOEXCEPT
 		: MyException( line, file ), error_( error ) {
+		whatBuffer_ += "[Error: " + std::to_string( error ) + "] "
+			+ desc.data( ) + "\n\n";
+
+		whatBuffer_ += std::system_category( ).message( error_ );
 	}
-}
+
+}	// namespace network
