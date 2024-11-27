62a169dc4e9d1873d733eb1a12737308f66cd14b
gnffk <114462674+gnffk@users.noreply.github.com>
2024-11-28 03:19:41 +0900

    Interface -> SendingStorage 변경

---
+++ Cuphead/include/SendingStorage.hpp
@@ -0,0 +1,36 @@
+#ifndef SENDING_STORAGE_HPP
+#define SENDING_STORAGE_HPP
+
+#include "define.hpp"
+#include "protocol.hpp"
+
+#include <atomic>
+#include <array>
+#include <mutex>
+#include <cstdint>
+
+class SendingStorage {
+	SINGLETON(SendingStorage);
+
+public:
+	void pushPacket(const Packet& packet);
+
+	void setFlag() {
+		flag_ = true;
+	}
+
+	void resetFlag() {
+		flag_ = false;
+	}
+
+	void copyTo(char* destBuffer);
+
+private:
+	std::atomic<bool> flag_;
+
+	std::array<char, BUFSIZE> buffer_;
+	std::uint16_t offset_;
+	std::mutex bufferMtx_;
+};
+
+#endif // SENDING_STORAGE_HPP
