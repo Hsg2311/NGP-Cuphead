83b9b131ddebb86c19d8b979090d34dbcc71ed62
orange2458 <asm1133@naver.com>
2024-11-28 02:42:31 +0900

    들여쓰기 수정

--- server/define.hpp
+++ server/define.hpp
@@ -1,19 +1,20 @@
 #ifndef DEFINE_HPP
 #define DEFINE_HPP
 
-#define SINGLETON(type) public:\
-							type( const type& ) = delete;\
-							type& operator=( const type& ) = delete;\
-							type( type&& ) = delete;\
-							type& operator=( type&& ) = delete;\
-							\
-							type& getInst( ) {\
-								static type inst;\
-								return inst;\
-							}\
-						\
-						private:\
-							type( );\
-							~type( )
+#define SINGLETON(type)\
+public:\
+	type( const type& ) = delete;\
+	type& operator=( const type& ) = delete;\
+	type( type&& ) = delete;\
+	type& operator=( type&& ) = delete;\
+	\
+	static type& getInst( ) {\
+		static type inst;\
+		return inst;\
+	}\
+	\
+private:\
+	type( );\
+	~type( )
 
 #endif // DEFINE_HPP
