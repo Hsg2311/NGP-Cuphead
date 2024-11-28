abe7ac6874127dba67f7cdf06e3b44808682cb6c
gnffk <114462674+gnffk@users.noreply.github.com>
2024-11-28 19:25:32 +0900

    send수정

--- Cuphead/src/ServerCL.cpp
+++ Cuphead/src/ServerCL.cpp
@@ -1,6 +1,7 @@
 #include "ServerCL.hpp"
 
 
+
 ServerCL::ServerCL()
 {
 }
@@ -22,56 +23,33 @@ void ServerCL::clientRecv(SOCKET sock)
 void ServerCL::clientSend(SOCKET sock)
 {
 	int retval;
+	static auto lastTp = Clock::now();
 	while (1) {
 
 		if (!SendingStorage::getInst().getFlag()) {
-            SendingStorage::getInst().resetFlag();
+            
+			auto tp = Clock::now();
+			auto elapsedTime = std::chrono::duration_cast<Seconds>(tp - lastTp);
+			lastTp = tp;
 
+			if (elapsedTime < (1_s / 30.f)) {
+				std::this_thread::sleep_for((1_s / 30.f) - elapsedTime);
+			}
            
             char destBuffer[BUFSIZE] = { 0 }; 
             std::uint16_t bufferSize;
 
-      
+			send(sock, &bufferSize, sizeof(bufferSize));
+			send(send, buffer.data(), bufferSize);
+
             SendingStorage::getInst().copyTo(destBuffer, bufferSize);
 
- 
-            ClientPacket packet;
-            std::memcpy(&packet, destBuffer, sizeof(ClientPacket));
-            
-            retval = send(sock, (char*)&packet.type, sizeof(ClientPacketType), 0);
+	
 
-            // 패킷 처리
-            switch (packet.type) {
-            case ClientPacketType::Input:
-                auto packetSize = sizeof(ClientPacketType) + sizeof(packet.Input);
+           
 
-                retval = send(sock, (char*)&packetSize, sizeof(packetSize), 0);
-                
-                retval = send(sock, (char*)&packet.Input, sizeof(packet.Input), 0);
-                break;
 
-            case ClientPacketType::LogIn:
-                auto packetSize = sizeof(ClientPacketType) + sizeof(packet.LogIn);
-
-                retval = send(sock, (char*)&packetSize, sizeof(packetSize), 0);
-
-                retval = send(sock, (char*)&packet.LogIn, sizeof(packet.LogIn), 0);
-               
-                break;
-
-            case ClientPacketType::LogOut:
-                auto packetSize = sizeof(ClientPacketType) + sizeof(packet.LogOut);
-
-                retval = send(sock, (char*)&packetSize, sizeof(packetSize), 0);
-
-                retval = send(sock, (char*)&packet.LogOut, sizeof(packet.LogOut), 0);
-               
-                break;
-
-            default:
-                
-                break;
-            }
+			SendingStorage::getInst().resetFlag();
 		}
 			
 	}
