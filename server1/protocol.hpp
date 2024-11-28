529075c872b22e9982c31684bb1dbb007b9d2879
parkshinehye <qkrtlsgp0996@gmail.com>
2024-11-28 23:47:31 +0900

    ㄴㄴㄴㄴ

---
+++ server1/protocol.hpp
@@ -0,0 +1,44 @@
+#ifndef PROTOCOL_HPP
+#define PROTOCOL_HPP
+
+#include "struct.hpp"
+#include <cstdint>
+
+constexpr short PORT = 9000;
+constexpr short BUFSIZE = 1024;
+
+enum class PacketType {
+	NONE,
+
+	LOGIN,
+	LEAVE,
+
+	MOVE,
+
+};
+
+enum class Direction {
+	NONE,
+
+	E, W, S, N,
+	NE, NW, SE, SW,
+};
+
+struct MovePacket {
+	std::uint8_t id;
+	Direction dir;
+	Vec2 pos;
+};
+
+struct Packet {
+	PacketType type;
+
+	union /*PacketData*/ {
+		MovePacket mv;
+	};
+
+	Packet( )
+		: type( PacketType::NONE ), mv( ) {}
+};
+
+#endif // PROTOCOL_HPP
