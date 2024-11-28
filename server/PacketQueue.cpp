68107f53aa2cb1154d9ce539682b8b8def259db0
orange2458 <asm1133@naver.com>
2024-11-29 04:23:51 +0900

    PacketQueue을 queue를 대체하여 사용하도록 변경

---
+++ server/PacketQueue.cpp
@@ -0,0 +1,73 @@
+#include "PacketQueue.hpp"
+#include "Timer.hpp"
+
+void PacketQueue::pushPacket( const Packet& packet ) {
+	std::lock_guard<std::mutex> lock( queueMtx_ );
+	packetQueue_.push( packet );
+}
+
+void PacketQueue::dispatch( ) {
+	while ( !packetQueue_.empty( ) ) {
+		std::lock_guard<std::mutex> lock( queueMtx_ );
+		Packet p = packetQueue_.front( );
+		packetQueue_.pop( );
+
+		switch ( p.type ) {
+		case PacketType::LOGIN:
+			//handleLoginPacket( p );
+			break;
+
+		case PacketType::INPUT: {
+			auto lu = Vec2( -1.f, 1.f );
+			auto ru = Vec2( 1.f, 1.f );
+			auto ld = Vec2( -1.f, -1.f );
+			auto rd = Vec2( 1.f, -1.f );
+
+			lu.normalize( );
+			ru.normalize( );
+			ld.normalize( );
+			rd.normalize( );
+
+			bool bUp = p.in.up;
+			bool bDown = p.in.down;
+			bool bLeft = p.in.left;
+			bool bRight = p.in.right;
+
+			if ( bUp && !bDown && !bLeft && !bRight ) {
+				objPos.y -= 300.f * Timer::getInst( ).getFDT( );
+			}
+			if ( bDown && !bUp && !bLeft && !bRight ) {
+				objPos.y += 300.f * Timer::getInst( ).getFDT( );
+			}
+			if ( bLeft && !bRight && !bUp && !bDown ) {
+				objPos.x -= 300.f * Timer::getInst( ).getFDT( );
+			}
+			if ( bRight && !bLeft && !bUp && !bDown ) {
+				objPos.x += 300.f * Timer::getInst( ).getFDT( );
+			}
+			if ( bLeft && bUp && !bDown && !bRight ) {
+				objPos.x -= 300.f * lu.x * Timer::getInst( ).getFDT( );
+				objPos.y -= 300.f * lu.y * Timer::getInst( ).getFDT( );
+			}
+			if ( bRight && bUp && !bDown && !bLeft ) {
+				objPos.x += 300.f * ru.x * Timer::getInst( ).getFDT( );
+				objPos.y -= 300.f * ru.y * Timer::getInst( ).getFDT( );
+			}
+			if ( bLeft && bDown && !bUp && !bRight ) {
+				objPos.x -= 300.f * ld.x * Timer::getInst( ).getFDT( );
+				objPos.y += 300.f * ld.y * Timer::getInst( ).getFDT( );
+			}
+			if ( bRight && bDown && !bUp && !bLeft ) {
+				objPos.x += 300.f * rd.x * Timer::getInst( ).getFDT( );
+				objPos.y += 300.f * rd.y * Timer::getInst( ).getFDT( );
+			}
+			break;
+		}
+		}
+	}
+}
+
+void PacketQueue::addObject( Object* obj ) {
+	networkIdToObject[ obj->getNetworkId( ) ] = obj;
+	objectToNetworkId[ obj ] = obj->getNetworkId( );
+}
---
+++ server/PacketQueue.hpp
@@ -0,0 +1,28 @@
+#ifndef PACKETQUEUE_HPP
+#define PACKETQUEUE_HPP
+
+#include "protocol.hpp"
+#include "Object.hpp"
+
+#include <queue>
+#include <mutex>
+#include <unordered_map>
+
+class PacketQueue {
+public:
+	void pushPacket( const Packet& packet );
+	void dispatch( );
+	void addObject( Object* obj );
+	Object* getObject( std::uint16_t id ) {
+		return networkIdToObject[ id ];
+	}
+
+private:
+	std::queue<Packet> packetQueue_;
+	std::mutex queueMtx_;
+
+	std::unordered_map<int, Object*> networkIdToObject;
+	std::unordered_map<Object*, int> objectToNetworkId;
+};
+
+#endif // PACKETQUEUE_HPP
