1e0577ff15ae34807314dfa67518e2e89edd1ae6
orange2458 <asm1133@naver.com>
2024-11-28 19:44:45 +0900

    move packet 설계

--- server/protocol.hpp
+++ server/protocol.hpp
@@ -1,19 +1,36 @@
 #ifndef PROTOCOL_HPP
 #define PROTOCOL_HPP
 
+#include "struct.hpp"
+#include <cstdint>
+
 constexpr short PORT = 9000;
 constexpr short BUFSIZE = 1024;
 
 enum class PacketType {
-	MOVE,
+	LOGIN,
 	LEAVE,
+
+	MOVE,
+
+};
+
+enum class Direction {
+	E, W, S, N,
+	NE, NW, SE, SW,
+};
+
+struct MovePacket {
+	std::uint8_t id;
+	Direction dir;
+	Vec2 pos;
 };
 
 struct Packet {
 	PacketType type;
 
-	union PacketData {
-
+	union /*PacketData*/ {
+		MovePacket mv;
 	};
 };
 
