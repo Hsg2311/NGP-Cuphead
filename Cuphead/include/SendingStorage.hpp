37e044bd4aa22846f79c162796ead27ea7a97b96
gnffk <114462674+gnffk@users.noreply.github.com>
2024-12-05 03:46:26 +0900

    서버코드

--- Cuphead/include/SendingStorage.hpp
+++ Cuphead/include/SendingStorage.hpp
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #ifndef SENDING_STORAGE_HPP
 #define SENDING_STORAGE_HPP
 
@@ -37,3 +38,32 @@ private:
 };
 
 #endif	// SENDING_STORAGE_HPP
+=======
+#ifndef INTERFACE_HPP
+#define INTERFACE_HPP
+
+#include "framework.h"
+#include "Protocol.hpp"
+#include "define.hpp"
+#include <thread>
+
+
+class Interface
+{
+	SINGLETON(Interface);
+public:
+	
+
+	ClientPacket packet;
+	void checkData() {
+		if (packet.type != ClientPacketType::None) { flag = TRUE; }
+	}
+
+	void setPacketInput(const ClientPacket& newPacket) { packet = newPacket; }
+
+	const ClientPacket& getPacketInput() const { return packet; }
+
+};
+
+#endif // INTERFACE_HPP
+>>>>>>> Stashed changes
--- Cuphead/src/SendingStorage.cpp
+++ Cuphead/src/SendingStorage.cpp
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #include "SendingStorage.hpp"
 
 #include <algorithm>
@@ -31,3 +32,7 @@ void SendingStorage::flush( char* destBuffer, std::uint16_t& bufferSize ) {
 	buffer_.fill( 0 );
 	bufferSize_ = 0;
 }
+=======
+#include "Interface.hpp"
+
+>>>>>>> Stashed changes
