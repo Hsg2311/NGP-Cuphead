c4dd3e0ccb43733cb21f9d14964dfefa4a9376f8
orange2458 <asm1133@naver.com>
2024-12-12 06:25:36 +0900

    server도 client와 마찬가지로 SceneHandler가 scene을 관리한다.

---
+++ server/SceneHandler.cpp
@@ -0,0 +1,27 @@
+#include "SceneHandler.hpp"
+#include "LobbyScene.hpp"
+#include "WorldScene.hpp"
+
+SceneHandler::SceneHandler( )
+	: sceneList_{ }
+	, currScene_{ nullptr }
+	, currSceneType_{ SCENE_TYPE::LOBBY_SCENE } {}
+
+SceneHandler::~SceneHandler( ) {
+	for ( auto& scene : sceneList_ ) {
+		delete scene;
+	}
+}
+
+void SceneHandler::init( ) {
+	sceneList_[ static_cast<UINT>( SCENE_TYPE::LOBBY_SCENE ) ] = new LobbyScene( );
+	sceneList_[ static_cast<UINT>( SCENE_TYPE::LOBBY_SCENE ) ]->setSceneName( L"Lobby Scene" );
+
+	sceneList_[ static_cast<UINT>( SCENE_TYPE::WORLD_SCENE ) ] = new WorldScene( );
+	sceneList_[ static_cast<UINT>( SCENE_TYPE::WORLD_SCENE ) ]->setSceneName( L"World Scene" );
+
+	// 현재 Scene 설정
+	currScene_ = sceneList_[ static_cast<UINT>( SCENE_TYPE::LOBBY_SCENE ) ];
+	currSceneType_ = SCENE_TYPE::LOBBY_SCENE;
+	currScene_->entry( );
+}
---
+++ server/SceneHandler.hpp
@@ -0,0 +1,43 @@
+#ifndef __SCENE_HANDLER_HPP
+#define __SCENE_HANDLER_HPP
+
+#include "define.hpp"
+#include "Scene.hpp"
+
+#include <array>
+
+class EventHandler;
+
+class SceneHandler {
+	SINGLETON( SceneHandler );
+
+public:
+	void init( );
+	void update( ) {
+		currScene_->update( );
+		currScene_->componentUpdate( );
+	}
+
+public:
+	Scene* getCurrScene( ) const { return currScene_; }
+	SCENE_TYPE getCurrSceneType( ) const { return currSceneType_; }
+
+private:
+	void changeScene( SCENE_TYPE sceneType ) {
+		currScene_->exit( );
+
+		currScene_ = sceneList_[ static_cast<UINT>( sceneType ) ];
+		currSceneType_ = sceneType;
+
+		currScene_->entry( );
+	}
+
+	friend class EventHandler;
+
+private:
+	std::array<Scene*, static_cast<UINT>( SCENE_TYPE::EOE )> sceneList_;
+	Scene* currScene_;
+	SCENE_TYPE currSceneType_;
+};
+
+#endif // __SCENE_HANDLER_HPP
